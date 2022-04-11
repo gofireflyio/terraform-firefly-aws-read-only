37e81f7fe5e34df38627faf945041366c3e61300
sefi-infralight <71797160+sefi-infralight@users.noreply.github.com>
2022-04-11 13:24:14 +0300

    Updating Firefly IAM Policy (11-04-2022) (#4)
    * Removing redundant friction of ExternalID from Firefly's IAM Role
    * Adding Deny List for resources Firefly doesn't need
    * Adding S3:GetObject for specific resources (*.tfstate)
    * Adding S3:GetObject for Buckets hold EMR and ElasticBeanstalk

--- outputs.tf
+++ outputs.tf
@@ -3,10 +3,6 @@ output "role_arn" {
   value = aws_iam_role.firefly_cross_account_access_role.arn
 }
 
-output "external_id" {
-  value = random_string.external_id.result
-}
-
 output "template_vesion" {
   value = local.version
 }
