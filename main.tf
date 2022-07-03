4bf8c51b325e30625898e51e5a6fe2a75a331880
tamirFireFly <101515629+tamirFireFly@users.noreply.github.com>
2022-07-03 12:30:34 +0300

    INFL-3165: Remove Integration On Destruction
    Send delete request to app server to delete the integration by its name and accountID

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 data "httpclient_request" "req" {
-  url = "${var.firefly_endpoint}/account/access_keys/login"
+  url             = "${var.firefly_endpoint}/account/access_keys/login"
   request_headers = {
-    Content-Type: "application/json",
+    Content-Type : "application/json",
   }
   request_method = "POST"
-  request_body = jsonencode({ "accessKey"=var.firefly_access_key,  "secretKey"=var.firefly_secret_key })
+  request_body   = jsonencode({ "accessKey" = var.firefly_access_key, "secretKey" = var.firefly_secret_key })
 }
 
 output "token" {
@@ -16,7 +16,10 @@ output "response_code" {
 }
 
 resource "time_sleep" "wait_10_seconds" {
-  depends_on = [aws_iam_policy.firefly_readonly_policy_deny_list, aws_iam_policy.firefly_s3_specific_write_permission, aws_iam_role.firefly_cross_account_access_role]
+  depends_on = [
+    aws_iam_policy.firefly_readonly_policy_deny_list, aws_iam_policy.firefly_s3_specific_write_permission,
+    aws_iam_role.firefly_cross_account_access_role
+  ]
 
   create_duration = "10s"
 }
@@ -24,6 +27,12 @@ resource "time_sleep" "wait_10_seconds" {
 resource "null_resource" "firefly_create_integration" {
   triggers = {
     version = local.version
+    token = data.httpclient_request.req.response_body
+    endpoint = var.firefly_endpoint
+    name  = var.name
+    full_scan_enabled = var.full_scan_enabled
+    is_prod = var.is_prod
+    role_external_id = var.role_external_id
   }
 
   provisioner "local-exec" {
@@ -36,5 +45,18 @@ curl --request POST "${var.firefly_endpoint}/integrations/aws/" \
 CURL
   }
 
-  depends_on = [aws_iam_policy.firefly_readonly_policy_deny_list, aws_iam_policy.firefly_s3_specific_write_permission, aws_iam_role.firefly_cross_account_access_role, time_sleep.wait_10_seconds]
+  provisioner "local-exec" {
+    when    = destroy
+    command = <<CURL
+curl --request DELETE "${self.triggers.endpoint}/integrations/aws/integration/name" \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${jsondecode(self.triggers.token).access_token}" \
+  --data ${jsonencode(jsonencode({"name"=self.triggers.name })) }
+CURL
+  }
+
+  depends_on = [
+    aws_iam_policy.firefly_readonly_policy_deny_list, aws_iam_policy.firefly_s3_specific_write_permission,
+    aws_iam_role.firefly_cross_account_access_role, time_sleep.wait_10_seconds
+  ]
 }
