39290c2ce74232419a331774b6e9e845ab8f77e5
galco5 <galco@gofirefly.io>
2022-08-01 20:26:15 +0300

    fixes

--- variables.tf
+++ variables.tf
@@ -48,12 +48,7 @@ variable event_driven_regions {
   default     = ["us-east-2", "us-east-1", "us-west-1", "us-west-2", "af-south-1", "ap-east-1", "ap-south-1", "ap-northeast-3", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2", "ap-northeast-1", "ca-central-1", "cn-north-1", "cn-northwest-1", "eu-central-1", "eu-west-1", "eu-west-2", "eu-south-1", "eu-west-3", "eu-north-1", "me-south-1", "sa-east-1"]
 }
 
-variable token_parameter_name {
-  type = string
-  default = "FireflyToken"
-}
-
 variable "target_event_bus_arn"{
   type = string
-  default = "arn:aws:events:us-west-2:094724549126:event-bus/stag-stablefly-event-bus"
+  default = "arn:aws:events:us-east-1:094724549126:event-bus/prod-stablefly-event-bus"
 }
