d356a4dc8494b556a3cdb3c5225ca35dad3fd743
Liav Yona <liav@infralight.co>
2022-09-12 14:02:35 +0300

    Add iac events input to new firefly integration

--- main.tf
+++ main.tf
@@ -153,7 +153,7 @@ module "firefly_aws_integration" {
 }
 
 module "firefly_eventbridge_permissions" {
-   count = var.is_event_driven ? 1 : 0
+  count = var.is_event_driven ? 1 : 0
   source = "./modules/eventbridge_permissions"
   target_event_bus_arn = var.target_event_bus_arn
   firefly_role_name = var.firefly_role_name
@@ -276,7 +276,6 @@ module "event_driven_ca_central_1" {
     aws = aws.ca_central_1
   }
 }
-
 
 module "event_driven_eu_central_1" {
   count = var.is_event_driven && contains(var.event_driven_regions, "eu-central-1") ? 1 : 0
@@ -433,6 +432,193 @@ module "event_driven_us_west_2" {
     module.firefly_aws_integration,
     module.firefly_eventbridge_permissions
   ]
+  providers      = {
+    aws = aws.us_west_2
+  }
+}
+
+module "iac_events_ap_northeast_1" {
+  count = lookup(var.buckets_by_region, "ap-northeast-1", []) != [] ? 1:0
+  source = "./modules/s3_iac_events"
+  bucket_names = lookup(var.buckets_by_region, "ap-northeast-1", [])
+  sns_arn = replace(var.iac_events_sns, "us-east-1", "ap-northeast-1")
+  region = "ap-northeast-1"
+  providers      = {
+    aws = aws.ap_northeast_1
+  }
+}
+
+module "iac_events_ap_northeast_2" {
+  count = lookup(var.buckets_by_region, "ap-northeast-2", []) != [] ? 1:0
+  source = "./modules/s3_iac_events"
+  bucket_names = lookup(var.buckets_by_region, "ap-northeast-2", [])
+  sns_arn = replace(var.iac_events_sns, "us-east-1", "ap-northeast-2")
+  region = "ap-northeast-2"
+  providers      = {
+    aws = aws.ap_northeast_2
+  }
+}
+
+module "iac_events_ap_northeast_3" {
+  count = lookup(var.buckets_by_region, "ap-northeast-3", []) != [] ? 1:0
+  source = "./modules/s3_iac_events"
+  bucket_names = lookup(var.buckets_by_region, "ap-northeast-3", [])
+  sns_arn = replace(var.iac_events_sns, "us-east-1", "ap-northeast-3")
+  region = "ap-northeast-3"
+  providers      = {
+    aws = aws.ap_northeast_3
+  }
+}
+
+module "iac_events_ap_south_1" {
+  count = lookup(var.buckets_by_region, "ap-south-1", []) != [] ? 1:0
+  source = "./modules/s3_iac_events"
+  bucket_names = lookup(var.buckets_by_region, "ap-south-1", [])
+  sns_arn = replace(var.iac_events_sns, "us-east-1", "ap-south-1")
+  region = "ap-south-1"
+  providers      = {
+    aws = aws.ap_south_1
+  }
+}
+
+module "iac_events_ap_southeast_1" {
+  count = lookup(var.buckets_by_region, "ap-southeast-1", []) != [] ? 1:0
+  source = "./modules/s3_iac_events"
+  bucket_names = lookup(var.buckets_by_region, "ap-southeast-1", [])
+  sns_arn = replace(var.iac_events_sns, "us-east-1", "ap-southeast-1")
+  region = "ap-southeast-1"
+  providers      = {
+    aws = aws.ap_southeast_1
+  }
+}
+
+module "iac_events_ap_southeast_2" {
+  count = lookup(var.buckets_by_region, "ap-southeast-2", []) != [] ? 1:0
+  source = "./modules/s3_iac_events"
+  bucket_names = lookup(var.buckets_by_region, "ap-southeast-2", [])
+  sns_arn = replace(var.iac_events_sns, "us-east-1", "ap-southeast-2")
+  region = "ap-southeast-2"
+  providers      = {
+    aws = aws.ap_southeast_2
+  }
+}
+
+module "iac_events_ca_central_1" {
+  count = lookup(var.buckets_by_region, "ca-central-1", []) != [] ? 1:0
+  source = "./modules/s3_iac_events"
+  bucket_names = lookup(var.buckets_by_region, "ca-central-1", [])
+  sns_arn = replace(var.iac_events_sns, "us-east-1", "ca-central-1")
+  region = "ca-central-1"
+  providers      = {
+    aws = aws.ca_central_1
+  }
+}
+
+module "iac_events_eu_central_1" {
+  count = lookup(var.buckets_by_region, "eu-central-1", []) != [] ? 1:0
+  source = "./modules/s3_iac_events"
+  bucket_names = lookup(var.buckets_by_region, "eu-central-1", [])
+  sns_arn = replace(var.iac_events_sns, "us-east-1", "eu-central-1")
+  region = "eu-central-1"
+  providers      = {
+    aws = aws.eu_central_1
+  }
+}
+
+module "iac_events_eu_north_1" {
+  count = lookup(var.buckets_by_region, "eu-north-1", []) != [] ? 1:0
+  source = "./modules/s3_iac_events"
+  bucket_names = lookup(var.buckets_by_region, "eu-north-1", [])
+  sns_arn = replace(var.iac_events_sns, "us-east-1", "eu-north-1")
+  region = "eu-north-1"
+  providers      = {
+    aws = aws.eu_north_1
+  }
+}
+
+module "iac_events_eu_west_1" {
+  count = lookup(var.buckets_by_region, "eu-west-1", []) != [] ? 1:0
+  source = "./modules/s3_iac_events"
+  bucket_names = lookup(var.buckets_by_region, "eu-west-1", [])
+  sns_arn = replace(var.iac_events_sns, "us-east-1", "eu-west-1")
+  region = "eu-west-1"
+  providers      = {
+    aws = aws.eu_west_1
+  }
+}
+
+module "iac_events_eu_west_2" {
+  count = lookup(var.buckets_by_region, "eu-west-2", []) != [] ? 1:0
+  source = "./modules/s3_iac_events"
+  bucket_names = lookup(var.buckets_by_region, "eu-west-2", [])
+  sns_arn = replace(var.iac_events_sns, "us-east-1", "eu-west-2")
+  region = "eu-west-2"
+  providers      = {
+    aws = aws.eu_west_2
+  }
+}
+
+module "iac_events_eu_west_3" {
+  count = lookup(var.buckets_by_region, "eu-west-3", []) != [] ? 1:0
+  source = "./modules/s3_iac_events"
+  bucket_names = lookup(var.buckets_by_region, "eu-west-3", [])
+  sns_arn = replace(var.iac_events_sns, "us-east-1", "eu-west-3")
+  region = "eu-west-3"
+  providers      = {
+    aws = aws.eu_west_3
+  }
+}
+
+module "iac_events_sa_east_1" {
+  count = lookup(var.buckets_by_region, "sa-east-1", []) != [] ? 1:0
+  source = "./modules/s3_iac_events"
+  bucket_names = lookup(var.buckets_by_region, "sa-east-1", [])
+  sns_arn = replace(var.iac_events_sns, "us-east-1", "sa-east-1")
+  region = "sa-east-1"
+  providers      = {
+    aws = aws.sa_east_1
+  }
+}
+
+module "iac_events_us_east_1" {
+  count = lookup(var.buckets_by_region, "us-east-1", []) != [] ? 1:0
+  source = "./modules/s3_iac_events"
+  bucket_names = lookup(var.buckets_by_region, "us-east-1", [])
+  sns_arn = var.iac_events_sns
+  region = "us-east-1"
+  providers      = {
+    aws = aws.us_east_1
+  }
+}
+
+module "iac_events_us_east_2" {
+  count = lookup(var.buckets_by_region, "us-east-2", []) != [] ? 1:0
+  source = "./modules/s3_iac_events"
+  bucket_names = lookup(var.buckets_by_region, "us-east-2", [])
+  sns_arn = replace(var.iac_events_sns, "us-east-1", "us-east-2")
+  region = "us-east-2"
+  providers      = {
+    aws = aws.us_east_2
+  }
+}
+
+module "iac_events_us_west_1" {
+  count = lookup(var.buckets_by_region, "us-west-1", []) != [] ? 1:0
+  source = "./modules/s3_iac_events"
+  bucket_names = lookup(var.buckets_by_region, "us-west-1", [])
+  sns_arn = replace(var.iac_events_sns, "us-east-1", "us-west-1")
+  region = "us-west-1"
+  providers      = {
+    aws = aws.us_west_1
+  }
+}
+
+module "iac_events_us_west_2" {
+  count = lookup(var.buckets_by_region, "us-west-2", []) != [] ? 1:0
+  source = "./modules/s3_iac_events"
+  bucket_names = lookup(var.buckets_by_region, "us-west-2", [])
+  sns_arn = replace(var.iac_events_sns, "us-east-1", "us-west-2")
+  region = "us-west-2"
   providers      = {
     aws = aws.us_west_2
   }
--- variables.tf
+++ variables.tf
@@ -36,9 +36,9 @@ variable exist_integration {
   description = "Whether the integration already exists or not"
 }
 
-variable "firefly_role_name"{
+variable "firefly_role_name" {
   default = "firefly-caa-role"
-  type = string
+  type    = string
 }
 
 variable full_scan_enabled {
@@ -56,35 +56,51 @@ variable is_event_driven {
 variable event_driven_regions {
   type        = list(string)
   description = "The list of regions to install firefly event driven in"
-  default     = ["us-east-2", "us-east-1", "us-west-1", "us-west-2", "af-south-1", "ap-east-1", "ap-south-1", "ap-northeast-3", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2", "ap-northeast-1", "ca-central-1", "cn-north-1", "cn-northwest-1", "eu-central-1", "eu-west-1", "eu-west-2", "eu-south-1", "eu-west-3", "eu-north-1", "me-south-1", "sa-east-1"]
+  default     = [
+    "us-east-2", "us-east-1", "us-west-1", "us-west-2", "af-south-1", "ap-east-1", "ap-south-1", "ap-northeast-3",
+    "ap-northeast-2", "ap-southeast-1", "ap-southeast-2", "ap-northeast-1", "ca-central-1", "cn-north-1",
+    "cn-northwest-1", "eu-central-1", "eu-west-1", "eu-west-2", "eu-south-1", "eu-west-3", "eu-north-1", "me-south-1",
+    "sa-east-1"
+  ]
 }
 
-variable "target_event_bus_arn"{
-  type = string
+variable "target_event_bus_arn" {
+  type    = string
   default = "arn:aws:events:us-east-1:094724549126:event-bus/prod-stablefly-event-bus"
 }
 
-variable "profile"{
+variable "profile" {
   default = ""
-  type = string
+  type    = string
 }
 
-variable "external_id"{
-  default= ""
-  type = string
+variable "external_id" {
+  default = ""
+  type    = string
 }
 
-variable "access_key"{
-  default= ""
-  type = string
+variable "access_key" {
+  default = ""
+  type    = string
 }
 
-variable "secret_key"{
-  default= ""
-  type = string
+variable "secret_key" {
+  default = ""
+  type    = string
 }
 
-variable "session_name"{
+variable "session_name" {
   default = "firefly"
-  type = string
+  type    = string
+}
+
+variable "buckets_by_region" {
+  type        = map(list(string))
+  description = ""
+  default     = {}
+}
+
+variable "iac_events_sns" {
+  default     = "arn:aws:sns:us-east-1:094724549126:tfstate-s3-updates"
+  description = "Firefly sns which receives s3 object events notification"
 }
