e80b21078691c8642d7a488e850fc7b47164aa21
YuvalFireFly <yuval@gofirefly.io>
2022-09-15 16:30:25 +0300

    Remove dot from tfstate read permission allow list

--- iam.tf
+++ iam.tf
@@ -21,7 +21,7 @@ resource "aws_iam_policy" "firefly_s3_specific_read_permission" {
         ],
         "Effect" : "Allow",
         "Resource" : [
-          "arn:aws:s3:::*/*.tfstate"
+          "arn:aws:s3:::*/*tfstate"
         ]
       },
       {
@@ -30,7 +30,7 @@ resource "aws_iam_policy" "firefly_s3_specific_read_permission" {
         ],
         "Effect" : "Allow",
         "Resource" : [
-          "arn:aws:s3:::*/*.tfstate",
+          "arn:aws:s3:::*/*tfstate",
           "arn:aws:s3:::elasticbeanstalk*/*",
           "arn:aws:s3:::aws-emr-resources*/*",
           "arn:aws:s3:::*/${data.aws_caller_identity.current.account_id}*ConfigSnapshot*.json.gz"
