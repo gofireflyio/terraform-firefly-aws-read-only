cda78f5c39b559f52a3660f90f4afa051776e726
YuvalFireFly <yuval@gofirefly.io>
2022-09-19 17:34:13 +0300

    Add id name for IaCathon event-driven notification

--- modules/s3_iac_events/main.tf
+++ modules/s3_iac_events/main.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket_notification" "aws_events_bucket_notification" {
-  name = "firefly-iac-states-update-notification"
   for_each = toset(var.bucket_names)
   bucket = each.key
   topic {
+    id            = "firefly-iac-states-update-notification"
     events        = ["s3:ObjectCreated:*", "s3:ObjectRemoved:*", "s3:ObjectRestore:Completed"]
     filter_suffix = var.suffix
     filter_prefix = var.prefix
