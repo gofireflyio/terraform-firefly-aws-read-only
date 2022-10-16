94b23a302d951077bbe7b99ea67364768d097be1
galco5 <galco@gofirefly.io>
2022-10-16 11:32:16 +0300

    resource login instead of data

--- modules/firefly_aws_integration/main.tf
+++ modules/firefly_aws_integration/main.tf
@@ -1,5 +1,5 @@
 
-data "terracurl_request" "firefly_login" {
+resource "terracurl_request" "firefly_login" {
   name           = "firefly_aws_integration"
   url            = "${var.firefly_endpoint}/account/access_keys/login"
   method         = "POST"
@@ -7,15 +7,25 @@ data "terracurl_request" "firefly_login" {
     Content-Type: "application/json",
   }
   request_body = jsonencode({ "accessKey"=var.firefly_access_key,  "secretKey"=var.firefly_secret_key })
+  response_codes = [200, 409]
 
+  destroy_url    = "${var.firefly_endpoint}/account/access_keys/login"
+  destroy_method = "POST"
+
+  destroy_headers = {
+    Content-Type: "application/json",
+  }
+
+  destroy_request_body =  jsonencode({ "accessKey"=var.firefly_access_key,  "secretKey"=var.firefly_secret_key })
+  destroy_response_codes = [204]
 }
 
 output "token" {
-  value = jsondecode(data.terracurl_request.firefly_login.response).access_token
+  value = jsondecode(terracurl_request.firefly_login.response).access_token
 }
 
 output "response_code" {
-  value = data.terracurl_request.firefly_login.response
+  value = terracurl_request.firefly_login.response
 }
 
 resource "time_sleep" "wait_10_seconds" {
@@ -44,10 +54,10 @@ resource "terracurl_request" "firefly_aws_integration_request" {
 
   headers = {
     Content-Type = "application/json"
-    Authorization: "Bearer ${jsondecode(data.terracurl_request.firefly_login.response).access_token}"
+    Authorization: "Bearer ${jsondecode(terracurl_request.firefly_login.response).access_token}"
   }
 
-   lifecycle {
+  lifecycle {
       ignore_changes = [
         headers,
         destroy_headers,
@@ -61,7 +71,7 @@ resource "terracurl_request" "firefly_aws_integration_request" {
 
   destroy_headers = {
     Content-Type = "application/json"
-    Authorization: "Bearer ${jsondecode(data.terracurl_request.firefly_login.response).access_token}"
+    Authorization: "Bearer ${jsondecode(terracurl_request.firefly_login.response).access_token}"
   }
 
   destroy_request_body =  jsonencode(
@@ -70,7 +80,7 @@ resource "terracurl_request" "firefly_aws_integration_request" {
     }
   )
   destroy_response_codes = [204]
-   depends_on = [
+  depends_on = [
     aws_iam_policy.firefly_readonly_policy_deny_list, aws_iam_policy.firefly_s3_specific_read_permission,
     aws_iam_role.firefly_cross_account_access_role, time_sleep.wait_10_seconds
   ]
